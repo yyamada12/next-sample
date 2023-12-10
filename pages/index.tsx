d177ec22f201d6d5f5e0d89e870b22f42cf1c7dc
yyamada12 <12yacropolisy@gmail.com>
2023-12-10 09:09:42 +0900

    confirm rendering behavior

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import react, { useState } from "react";
 import styled from "styled-components";
 
 const Text = styled.span`
@@ -7,11 +8,56 @@ const Text = styled.span`
 `;
 
 const Home: NextPage = () => {
+  const [text, updateText] = useState("");
+  const C2 = react.memo(Component2);
   return (
     <div>
-      <Text>Themeから参照した色を使用しています</Text>
+      <Component1A text={text} updateText={updateText} />
+      <Component1B />
+      <C2 />
     </div>
   );
+};
+
+const Component1A = ({ text, updateText }) => {
+  return (
+    <div style={{ margin: "20px" }}>
+      component1
+      <form>
+        <textarea
+          name="text"
+          id="text"
+          cols={30}
+          rows={10}
+          value={text}
+          onChange={(e) => updateText(e.target.value)}
+        />
+      </form>
+    </div>
+  );
+};
+
+const Component1B = () => {
+  const [text, updateText] = useState("");
+  return (
+    <div style={{ margin: "20px" }}>
+      component1
+      <form>
+        <textarea
+          name="text"
+          id="text"
+          cols={30}
+          rows={10}
+          value={text}
+          onChange={(e) => updateText(e.target.value)}
+        />
+      </form>
+    </div>
+  );
+};
+
+const Component2 = () => {
+  return <div style={{ margin: "20px" }}>component2</div>;
 };
 
 export default Home;
